42a23e1bb2139a9a3419a0976916c1779166a6cf
EduardoSalvadorNique <esalvadn@emeal.nttdata.com>
2026-02-11 19:30:56 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
+import os
 from fastmcp import FastMCP
 
 mcp = FastMCP("Calculadora MCP Básica")
 
 @mcp.tool(name="sumar")
 def sumar(a: float, b: float) -> float:
-    """Suma dos números y devuelve el resultado."""
     return a + b
 
 @mcp.tool(name="restar")
 def restar(a: float, b: float) -> float:
-    """Resta dos números y devuelve el resultado."""
     return a - b
 
 if __name__ == "__main__":
-    # Levanta un servidor HTTP 
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    # Usa el puerto que Render asigna automáticamente
+    port = int(os.environ.get("PORT", 8000))
+    mcp.run(transport="http", host="0.0.0.0", port=port)
